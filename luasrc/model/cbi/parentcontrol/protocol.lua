0e9d55308a32f1bd2a051a159b606fd9c8710551
Kristiyan Georgiev <127213270+b-r-a-v-e-o-n-e@users.noreply.github.com>
2023-12-26 21:38:24 +0100

    Update protocol.lua

--- luasrc/model/cbi/parentcontrol/protocol.lua
+++ luasrc/model/cbi/parentcontrol/protocol.lua
@@ -4,47 +4,47 @@ local ipc = require "luci.ip"
 local net = require "luci.model.network".init()
 local sys = require "luci.sys"
 local a, t, e
-a = Map("parentcontrol", translate("Parent Control"), translate("<b><font color=\"green\">利用iptables来管控数据包过滤以禁止符合设定条件的用户连接互联网的工具软件。</font> </b></br>\
-协议过滤：可以控制指定MAC机器是否使用指定协议端口，包括IPV4和IPV6，端口可以是连续端口范围用冒号分隔如5000:5100或多个端口用逗号分隔如：5100,5110,5001:5002,440:443</br>不指定MAC就是代表限制所有机器,起控时间要小于停控时间，不指定时间表示时段" ))
+a = Map("parentcontrol", translate("Parent Control"), translate("<b><font color=\"green\">Use iptables Tool software to control packet filtering and prohibit users who meet set conditions from connecting to the Internet.</font> </b></br>\
+Protocol filtering: Can control whether the specified MAC machine uses the specified protocol port, include IPV4 and IPV6，Ports can be consecutive port ranges separated by colons such as 5000:5100 or multiple ports separated by commas such as: 5100, 5110, 5001:5002, 440:443</br>Not specify MAC it means restricting all machines, The start control time should be less than the stop control time， Express period without specifying time" ))
 
 a.template = "parentcontrol/index"
 t = a:section(TypedSection, "basic", translate(""))
 t.anonymous = true
-e = t:option(DummyValue, "parentcontrol_status", translate("当前状态"))
+e = t:option(DummyValue, "parentcontrol_status", translate("Current status"))
 e.template = "parentcontrol/parentcontrol"
 e.value = translate("Collecting data...")
 
-e = t:option(Flag, "enabled", translate("开启"))
+e = t:option(Flag, "enabled", translate("Turn on"))
 e.rmempty = false
 
-e = t:option(ListValue, "algos", translate("过滤力度"))
-e:value("bm", "一般过滤")
-e:value("kmp", "强效过滤")
+e = t:option(ListValue, "algos", translate("Filtration strength"))
+e:value("bm", "General filtering")
+e:value("kmp", "Powerful filtering")
 e.default = "kmp"
 
-e = t:option(ListValue, "control_mode",translate("限制模式"), translate("黑名单模式，列表中的客户端设置将被禁止；白名单模式：仅有列表中的客户端设置允许。"))
+e = t:option(ListValue, "control_mode",translate("Restricted mode"), translate("Blacklist mode, Client settings in the list will be disabled；Whitelist mode: Only client settings in the list are allowed."))
 e.rmempty = false
-e:value("white_mode", "白名单")
-e:value("black_mode", "黑名单")
+e:value("white_mode", "Whitelist")
+e:value("black_mode", "Blacklist")
 e.default = "black_mode"
 
-t = a:section(TypedSection, "protocol", translate("协议过滤"))
+t = a:section(TypedSection, "protocol", translate("Protocol filtering"))
 t.template = "cbi/tblsection"
 t.anonymous = true
 t.addremove = true
-e = t:option(Value, "mac", translate("MAC地址<font color=\"green\">(留空则过滤全部客户端)</font>"))
+e = t:option(Value, "mac", translate("MAC address <font color=\"green\">(Leave blank to filter all clients)</font>"))
 e.placeholder = "ALL"
 e.rmempty = true
 o.net.mac_hints(function(t, a) e:value(t, "%s (%s)" % {t, a}) end)
-e = t:option(ListValue, "proto", translate("<font color=\"gray\">端口协议</font>"))
+e = t:option(ListValue, "proto", translate("<font color=\"gray\">Port protocol</font>"))
 e.rmempty = false
 e.default = 'tcp'
 e:value("tcp", translate("TCP"))
 e:value("udp", translate("UDP"))
 e:value("icmp", translate("ICMP"))
-e = t:option(Value, "ports", translate("<font color=\"gray\">源端口</font>"))
+e = t:option(Value, "ports", translate("<font color=\"gray\">Source port</font>"))
 e.rmempty = true
-e = t:option(Value, "portd", translate("<font color=\"gray\">目的端口</font>"))
+e = t:option(Value, "portd", translate("<font color=\"gray\">Destination port</font>"))
 e:value("",translate("ICMP"))
 e:value("80", "TCP-HTTP")
 e:value("443", "TCP-HTTPS")
@@ -70,15 +70,15 @@ e.rmempty = true
         if hh and mm and hh <= 23 and mm <= 59 then
             return value
         else
-            return nil, "时间格式必须为 HH:MM 或者留空"
+            return nil, "The time format must be HH:MM Or leave it blank"
         end
     end
-e = t:option(Value, "timestart", translate("起控时间"))
+e = t:option(Value, "timestart", translate("Start time"))
 e.placeholder = '00:00'
 e.default = '00:00'
 e.validate = validate_time
 e.rmempty = true
-e = t:option(Value, "timeend", translate("停控时间"))
+e = t:option(Value, "timeend", translate("Stop time"))
 e.placeholder = '00:00'
 e.default = '00:00'
 e.validate = validate_time
@@ -97,7 +97,7 @@ week:value(6,translate("Saturday"))
 week.default='*'
 
 
-e = t:option(Flag, "enable", translate("开启"))
+e = t:option(Flag, "enable", translate("Turn on"))
 e.rmempty = false
 e.default = '1'
 
@@ -107,6 +107,5 @@ a.on_after_apply = function(self,map)
 end
 
 return a
-
 
 
